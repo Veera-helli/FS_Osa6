c303e80508f2834fee727ecc822b734e10752b8d
Veera Ihalainen <Veera.helli@gmail.com>
2022-08-22 12:16:39 +0300

    6.21 notification timeout bug ixes

--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,31 +1,39 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = null;
+const initialState = {};
 
 const notifSlice = createSlice({
   name: 'notif',
   initialState,
   reducers: {
     setNotif(state, action) {
-      const newNotif = action.payload;
-      return newNotif;
+      return { ...state, notif: action.payload };
     },
     removeNotif(state, action) {
-      return null;
+      return { ...state, notif: null };
+    },
+    setID(state, action) {
+      return { ...state, id: action.payload };
+    },
+    clearID(state, action) {
+      clearTimeout(state.id);
+      return { ...state, id: null };
     },
   },
 });
 
-export const { setNotif, removeNotif } = notifSlice.actions;
+export const { setNotif, removeNotif, setID, clearID } = notifSlice.actions;
 
 export const setNotification = (content, seconds) => {
   return (dispatch) => {
+    dispatch(clearID());
     const time = 1000 * seconds;
     dispatch(setNotif(content));
-    setTimeout(() => {
+    const id = setTimeout(() => {
       console.log('In timeout');
       dispatch(removeNotif());
     }, time);
+    dispatch(setID(id));
   };
 };
 
